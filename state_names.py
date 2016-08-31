ff15b9bfe2c2b3201a3c64f1eab1db33e3d3517d
ramfay <candy_random@live.com.au>
2016-09-01 09:52:32 +1000

    Complete state names

--- state_names.py
+++ state_names.py
@@ -6,12 +6,15 @@ File needs reformatting
 
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-# print(STATE_NAMES)
+print(STATE_NAMES)
 
-state = input("Enter short state: ")
-while state != "":
-    if state in STATE_NAMES:
-        print(state, "is", STATE_NAMES[state])
-    else:
-        print("Invalid short state")
-    state = input("Enter short state: ")
+# state = input("Enter short state: ").upper()
+# while state != "":
+#     if state in STATE_NAMES:
+#         print(state, "is", STATE_NAMES[state])
+#     else:
+#         print("Invalid short state")
+#     state = input("Enter short state: ").upper()
+
+for key, value in STATE_NAMES.items():
+    print("{:3} is {:2}".format(key, value))
